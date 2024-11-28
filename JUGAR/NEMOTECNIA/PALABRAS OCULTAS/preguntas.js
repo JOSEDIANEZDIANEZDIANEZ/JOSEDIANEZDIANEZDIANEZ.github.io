73166d2a1a797653c92576be419b9514e6b3da25
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2024-11-28 11:42:22 +0100

    Add files via upload

---
+++ JUGAR/NEMOTECNIA/PALABRAS OCULTAS/preguntas.js
@@ -0,0 +1,2 @@
+var text = "España es un estado democrático de derecho que busca por encima de su ordenamiento juridico la libertad , la igualdad y el pluralismo político.La soberanía nacional recae sobre el pueblo español del cual emanan los poderes del Estado .";
+var keywords = ["democrático", "derecho", "libertad", "igualdad", "pluralismo", "político","España","soberanía","pueblo","poderes"];
