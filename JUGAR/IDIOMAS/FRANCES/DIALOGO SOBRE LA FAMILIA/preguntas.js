7fdfb71538d3f3ee1c29e415d6f111da0debeab8
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2024-11-28 11:27:32 +0100

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/DIALOGO SOBRE LA FAMILIA/preguntas.js
@@ -0,0 +1,102 @@
+
+
+
+
+
+var preguntas = [
+    {
+        "pregunta": "Tu as des freres et des soeurs?",
+        "respuesta": "¿Tienes hermanos y hermanas?"
+    },
+
+
+    {
+        "pregunta": "Je n'ai pas de sœurs, mais j'ai un frère qui s'appelle Samuel.",
+        "respuesta": "No tengo hermanas, pero tengo un hermano que se llama Samuel."
+    },
+
+    {
+        "pregunta": "Ils ant quel âge?",
+        "respuesta": "¿Qué edad tienen?"
+    },
+
+    {
+        "pregunta": "Il a sept ans et il est plus jeune que moi.",
+        "respuesta": "Tiene siete años y es más joven que yo."
+    },
+
+    {
+        "pregunta": "Quelles langues pales-tu?avec qui?",
+        "respuesta": "Que idiomas hablas con quien?"
+    },
+
+    {
+        "pregunta": "Je parle anglais avec ma mère, mon père et mon oncle.",
+        "respuesta": "Hablo inglés con mi madre, mi padre y mi tío."
+    },
+
+
+
+    {
+        "pregunta": "Est-ce que tes parents travaillent?",
+        "respuesta": "¿tus padres trabajan?"
+    },
+
+
+    {
+        "pregunta": "Oui, ils travaillent.",
+        "respuesta": "Sí, ellos trabajan."
+    },
+
+
+    {
+        "pregunta": "Qu'est qu'ils font?",
+        "respuesta": "¿Qué están haciendo?"
+    },
+
+    {
+        "pregunta": "Mon père travaille a Carrefour et ma mère est assistante sociale et travaille à Cadix.",
+        "respuesta": "Mi padre trabaja en Carrefour y mi madre es trabajadora social y trabaja en Cádiz."
+    },
+
+
+    {
+        "pregunta": "Tu as un animal?",
+        "respuesta": "¿tienes un animal?"
+    },
+
+    {
+        "pregunta": "Non, mais ma tante en a un.",
+        "respuesta": "No, pero mi tía sí lo tiene."
+    },
+
+    {
+        "pregunta": "Qu'est-ce quec'est?",
+        "respuesta": "¿Que es?"
+    },
+    {
+        "pregunta": "C'est un chien.",
+        "respuesta": "Es un perro."
+    },
+
+    {
+        "pregunta": "Comment iles'apelle?",
+        "respuesta": "¿Como se llama?"
+    },
+
+
+    {
+        "pregunta": "Elle s'appelle Cloé.",
+        "respuesta": "Se llama Cloé."
+    }
+
+
+
+
+
+
+];
+
+
+
+
