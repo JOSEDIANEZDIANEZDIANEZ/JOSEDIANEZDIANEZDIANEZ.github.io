7e24c0e47405aca5c37c1ea4f414794af9c170d3
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2024-11-28 13:22:06 +0100

    Add files via upload

---
+++ JUGAR/LENGUA/ADVERBIOS/preguntas.js
@@ -0,0 +1,269 @@
+var preguntas = [
+
+    {
+        "pregunta": "DETRAS",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+    {
+        "pregunta": "PARA",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "MEDIANTE",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "MAL",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+
+    {
+        "pregunta": "DESPUES",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+
+
+    {
+        "pregunta": "ASI",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+
+    {
+        "pregunta": "DESPACIO",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+    {
+        "pregunta": "TARDE",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+
+
+
+    {
+        "pregunta": "AHORA",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+
+
+    {
+        "pregunta": "MAÑANA",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+
+    {
+        "pregunta": "DENTRO",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+    {
+        "pregunta": "RAPIDAMENTE",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+    {
+        "pregunta": "COMODAMENTE",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+    {
+        "pregunta": "BIEN",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+
+    {
+        "pregunta": "SEGUN",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+    {
+        "pregunta": "NI",
+        "respuesta": "CONJUNCIÓN"
+    },
+
+    {
+        "pregunta": "PERO",
+        "respuesta": "CONJUNCIÓN"
+    },
+    {
+        "pregunta": "BAJO",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+
+    {
+        "pregunta": "SIN",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+    {
+        "pregunta": "DESDE",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+    {
+        "pregunta": "ANTE",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "POR",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "NUNCA",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+    {
+        "pregunta": "SIEMPRE",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+
+    {
+        "pregunta": "DURANTE",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+    {
+        "pregunta": "FUERA",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+
+    {
+        "pregunta": "ARRIBA",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+    {
+        "pregunta": "DE",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+
+    {
+        "pregunta": "O",
+        "respuesta": "CONJUNCIÓN"
+    },
+
+
+    {
+        "pregunta": "ALLI",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+
+    {
+        "pregunta": "DELANTE",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+    {
+        "pregunta": "CON",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "AQUI",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+    {
+        "pregunta": "CERCA",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+
+    {
+        "pregunta": "HACIA",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+    {
+        "pregunta": "I",
+        "respuesta": "CONJUNCIÓN"
+    },
+
+    {
+        "pregunta": "TODAS LAS PALABRAS QUE TERMINEN EN MENTE",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+    {
+        "pregunta": "ANTES",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+
+
+
+    {
+        "pregunta": "A",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+
+    {
+        "pregunta": "HOY",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+    {
+        "pregunta": "AUNQUE",
+        "respuesta": "CONJUNCIÓN"
+    },
+    {
+        "pregunta": "AYER",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+    {
+        "pregunta": "PRONTO",
+        "respuesta": "ADVERBIO DE TIEMPO"
+    },
+    {
+        "pregunta": "ENTRE",
+        "respuesta": "PREPOSICIÓN"
+    },
+
+    {
+        "pregunta": "E",
+        "respuesta": "CONJUNCIÓN"
+    },
+
+
+    {
+        "pregunta": "ABAJO",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+
+    {
+        "pregunta": "SIN EMBARGO",
+        "respuesta": "CONJUNCIÓN"
+    },
+    {
+        "pregunta": "LEJOS",
+        "respuesta": "ADVERBIO DE LUGAR"
+    },
+
+
+
+    {
+        "pregunta": "REGULAR",
+        "respuesta": "ADVERBIO DE MODO"
+    },
+
+
+    {
+        "pregunta": "EN",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "CONTRA",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "SOBRE",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "TRAS",
+        "respuesta": "PREPOSICIÓN"
+    },
+    {
+        "pregunta": "HASTA",
+        "respuesta": "PREPOSICIÓN"
+    }
+
+    
+];
