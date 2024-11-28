a18e77a6e28af6d0acb73bad71014c0070df2aca
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2024-11-28 11:13:51 +0100

    Add files via upload

---
+++ JUGAR/GEOGRAFIA/CAPITALES/CAPITALES DE EUROPA DUELO/preguntas.js
@@ -0,0 +1,312 @@
+var nombreDelJuego = "CAPITALES DE EUROPA";
+
+var absolutamentetodaslaspreguntas = [
+    {
+        "pregunta": "¿Cuál es la capital de Alemania?",
+        "respuesta": "Berlín",
+        "palabrasNecesarias": "berlin",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Andorra?",
+        "respuesta": "Andorra la Vella",
+        "palabrasNecesarias": "andorra la vella",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Austria?",
+        "respuesta": "Viena",
+        "palabrasNecesarias": "viena",
+        "Ladoyporbuena": "-,bien-viena",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Bélgica?",
+        "respuesta": "Bruselas",
+        "palabrasNecesarias": "bruselas",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Bielorrusia?",
+        "respuesta": "Minsk",
+        "palabrasNecesarias": "minsk",
+        "Ladoyporbuena": "-,minds-minsk,mix-minsk",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Bulgaria?",
+        "respuesta": "Sofía",
+        "palabrasNecesarias": "sofia",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Chipre?",
+        "respuesta": "Nicosia",
+        "palabrasNecesarias": "nicosia",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Croacia?",
+        "respuesta": "Zagreb",
+        "palabrasNecesarias": "zagreb",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Dinamarca?",
+        "respuesta": "Copenhague",
+        "palabrasNecesarias": "copenhague",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Eslovaquia?",
+        "respuesta": "Bratislava",
+        "palabrasNecesarias": "bratislava",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Eslovenia?",
+        "respuesta": "Liubliana",
+        "palabrasNecesarias": "liubliana",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de España?",
+        "respuesta": "Madrid",
+        "palabrasNecesarias": "madrid",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Estonia?",
+        "respuesta": "Tallin",
+        "palabrasNecesarias": "tallin",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Finlandia?",
+        "respuesta": "Helsinki",
+        "palabrasNecesarias": "helsinki",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Francia?",
+        "respuesta": "París",
+        "palabrasNecesarias": "paris",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Grecia?",
+        "respuesta": "Atenas",
+        "palabrasNecesarias": "atenas",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Hungría?",
+        "respuesta": "Budapest",
+        "palabrasNecesarias": "budapest",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Irlanda?",
+        "respuesta": "Dublín",
+        "palabrasNecesarias": "dublin",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Islandia?",
+        "respuesta": "Reikiavik",
+        "palabrasNecesarias": "reikiavik",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Italia?",
+        "respuesta": "Roma",
+        "palabrasNecesarias": "roma",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Kosovo?",
+        "respuesta": "Pristina",
+        "palabrasNecesarias": "pristina",
+        "Ladoyporbuena": "-,cristina-pristina",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Letonia?",
+        "respuesta": "Riga",
+        "palabrasNecesarias": "riga",
+        "Ladoyporbuena": "-,rica-riga",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Liechtenstein?",
+        "respuesta": "Vaduz",
+        "palabrasNecesarias": "vaduz",
+        "Ladoyporbuena": "-,badoo-vaduz",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Lituania?",
+        "respuesta": "Vilna",
+        "palabrasNecesarias": "vilna",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Luxemburgo?",
+        "respuesta": "Luxemburgo",
+        "palabrasNecesarias": "luxemburgo",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Macedonia del Norte?",
+        "respuesta": "Skopie",
+        "palabrasNecesarias": "skopie",
+        "Ladoyporbuena": "-,scopie-skopie,scopia-skopie",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Malta?",
+        "respuesta": "La Valeta",
+        "palabrasNecesarias": "la valeta",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Moldavia?",
+        "respuesta": "Chisináu",
+        "palabrasNecesarias": "chisinau",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Mónaco?",
+        "respuesta": "Mónaco",
+        "palabrasNecesarias": "monaco",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Montenegro?",
+        "respuesta": "Podgorica",
+        "palabrasNecesarias": "podgorica",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Noruega?",
+        "respuesta": "Oslo",
+        "palabrasNecesarias": "oslo",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Países Bajos?",
+        "respuesta": "Ámsterdam",
+        "palabrasNecesarias": "amsterdam",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Polonia?",
+        "respuesta": "Varsovia",
+        "palabrasNecesarias": "varsovia",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Portugal?",
+        "respuesta": "Lisboa",
+        "palabrasNecesarias": "lisboa",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Reino Unido?",
+        "respuesta": "Londres",
+        "palabrasNecesarias": "londres",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de República Checa?",
+        "respuesta": "Praga",
+        "palabrasNecesarias": "praga",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Rumania?",
+        "respuesta": "Bucarest",
+        "palabrasNecesarias": "bucarest",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Rusia?",
+        "respuesta": "Moscú",
+        "palabrasNecesarias": "moscu",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de San Marino?",
+        "respuesta": "San Marino",
+        "palabrasNecesarias": "san marino",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Serbia?",
+        "respuesta": "Belgrado",
+        "palabrasNecesarias": "belgrado",
+        "Ladoyporbuena": "-,grado-belgrado",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Suecia?",
+        "respuesta": "Estocolmo",
+        "palabrasNecesarias": "estocolmo",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Suiza?",
+        "respuesta": "Berna",
+        "palabrasNecesarias": "berna",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Ucrania?",
+        "respuesta": "Kiev",
+        "palabrasNecesarias": "kiev",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "¿Cuál es la capital de Vaticano?",
+        "respuesta": "Ciudad del Vaticano",
+        "palabrasNecesarias": "ciudad del vaticano",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    }
+];
