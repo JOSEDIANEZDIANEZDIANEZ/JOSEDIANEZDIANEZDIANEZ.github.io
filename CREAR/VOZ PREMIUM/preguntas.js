5b68b9bd04c90a680252e6e936f5517fde46532f
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2024-11-30 15:24:55 +0100

    Add files via upload

---
+++ CREAR/VOZ PREMIUM/preguntas.js
@@ -0,0 +1,95 @@
+var nombreDelJuego = "ARTICULO 1 CONSTITUCION ESPAÑOLA";
+
+var absolutamentetodaslaspreguntas = [
+    {
+        "pregunta": "Estaba sucio",
+        "respuesta": "Estado social",
+        "palabrasNecesarias": "estado social",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Demonio derecho",
+        "respuesta": "Democratico de derecho",
+        "palabrasNecesarias": "democratico derecho",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Pulpo",
+        "respuesta": "Pluralismo político",
+        "palabrasNecesarias": "pluralismo politico",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Juli",
+        "respuesta": "Justicia y libertad",
+        "palabrasNecesarias": "justicia libertad",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Higos",
+        "respuesta": "Igualdad",
+        "palabrasNecesarias": "igualdad",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Soberano",
+        "respuesta": "Soberanía",
+        "palabrasNecesarias": "soberania",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Pueblo",
+        "respuesta": "Pueblo",
+        "palabrasNecesarias": "pueblo",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Vomita",
+        "respuesta": "Emanan",
+        "palabrasNecesarias": "emanan",
+        "Ladoyporbuena": "-,hermana-emanan",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Powder",
+        "respuesta": "Poderes",
+        "palabrasNecesarias": "poderes",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "En Estado",
+        "respuesta": "Estado",
+        "palabrasNecesarias": "estado",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "En forma",
+        "respuesta": "La forma",
+        "palabrasNecesarias": "forma",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Mona",
+        "respuesta": "Monarquía",
+        "palabrasNecesarias": "monarquia",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    },
+    {
+        "pregunta": "Habla",
+        "respuesta": "Parla",
+        "palabrasNecesarias": "parla",
+        "Ladoyporbuena": "-",
+        "reglanemotecnica": ""
+    }
+];
