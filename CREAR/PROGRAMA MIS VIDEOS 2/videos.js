7f4b2a06f066d768bb6c3791e1af7e0800873cd7
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2024-11-30 15:24:10 +0100

    Add files via upload

---
+++ CREAR/PROGRAMA MIS VIDEOS 2/videos.js
@@ -0,0 +1,22 @@
+const videos = [
+  {
+    "title": "La mamá y el hijo (parte 1)",
+    "url": "https://www.youtube.com/watch?v=d3Uro-Zeh-g",
+    "thumbnail": "https://img.youtube.com/vi/d3Uro-Zeh-g/0.jpg"
+  },
+  {
+    "title": "¡¡ANUNCIANDO AL GANADOR DEL SORTEO DEL TITAN CLOVER MAN!! (Toilet tower defense)",
+    "url": "https://www.youtube.com/watch?v=XDQJd-za1Sw",
+    "thumbnail": "https://img.youtube.com/vi/XDQJd-za1Sw/0.jpg"
+  },
+  {
+    "title": "¡¡NUEVOS SPOILERS DE SKIBIDI TOILET 77 PARTE 3!! ¿El significado del 10124 cuál es??",
+    "url": "https://www.youtube.com/watch?v=2dPr1G6x-bY",
+    "thumbnail": "https://img.youtube.com/vi/2dPr1G6x-bY/0.jpg"
+  },
+  {
+    "title": "TRAILER STAR WAR I",
+    "url": "https://www.youtube.com/watch?v=bD7bpG-zDJQ",
+    "thumbnail": "https://img.youtube.com/vi/bD7bpG-zDJQ/0.jpg"
+  }
+];
