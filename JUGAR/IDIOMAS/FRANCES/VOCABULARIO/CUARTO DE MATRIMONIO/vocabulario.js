fce6af899b886fd84382428cf5f5d08bdb8ae533
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-05-04 13:13:18 +0200

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/VOCABULARIO/CUARTO DE MATRIMONIO/vocabulario.js
@@ -0,0 +1,6 @@
+const vocabulary = [
+    { spanish: "el cuarto de matrimonio", french: "la chambre parentale" },
+    { spanish: "mesita de noche", french: "table de chevet" },
+    { spanish: "ropero", french: "armoire" },
+    { spanish: "escaleras", french: "escalier" }
+];
