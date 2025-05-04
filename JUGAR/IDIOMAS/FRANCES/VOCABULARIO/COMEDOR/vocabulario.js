211d0dc77994b876083ba66123fae7d71fb097c4
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-05-04 13:12:53 +0200

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/VOCABULARIO/COMEDOR/vocabulario.js
@@ -0,0 +1,8 @@
+const vocabulary = [
+    { spanish: "comedor", french: "salle à manger" },
+    { spanish: "mesa", french: "table" },
+    { spanish: "silla", french: "chaise" },
+    { spanish: "ventana", french: "fenêtre" },
+    { spanish: "puerta", french: "porte" },
+    { spanish: "pared", french: "mur" }
+];
