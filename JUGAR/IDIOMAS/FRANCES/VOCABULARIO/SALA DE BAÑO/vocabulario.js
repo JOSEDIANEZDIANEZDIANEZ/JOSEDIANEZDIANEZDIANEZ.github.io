1589e75c441f6d3e51b916c481b4095f4daf2440
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-05-04 13:14:15 +0200

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/VOCABULARIO/SALA DE BAÑO/vocabulario.js
@@ -0,0 +1,10 @@
+const vocabulary = [
+    { spanish: "suelo", french: "sol" },
+    { spanish: "recibidor", french: "entrée" },
+    { spanish: "interruptor", french: "interrupteur" },
+    { spanish: "enchufe", french: "prise" },
+    { spanish: "la sala de baño", french: "la salle de bain" },
+    { spanish: "bañera", french: "baignoire" },
+    { spanish: "ducha", french: "douche" },
+    { spanish: "lavabo", french: "lavabo" }
+];
