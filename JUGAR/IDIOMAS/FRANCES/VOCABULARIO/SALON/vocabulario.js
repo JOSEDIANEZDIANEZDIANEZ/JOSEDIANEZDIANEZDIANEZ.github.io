e8750c40a0a2e23eab475b55655f58d92414289c
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-05-04 13:14:42 +0200

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/VOCABULARIO/SALON/vocabulario.js
@@ -0,0 +1,9 @@
+const vocabulary = [
+    { spanish: "salón", french: "salon" },
+    { spanish: "sillón", french: "fauteuil" },
+    { spanish: "televisor", french: "téléviseur" },
+    { spanish: "estantería", french: "étagère" },
+    { spanish: "mueble bar", french: "meuble bar" },
+    { spanish: "cuadro", french: "tableau" },
+    { spanish: "lámpara", french: "lampe" }
+];
