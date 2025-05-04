f67942636e081474380077c0d87b3ffec98797bc
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-05-04 13:13:47 +0200

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/VOCABULARIO/HABITACION/vocabulario.js
@@ -0,0 +1,11 @@
+const vocabulary = [
+    { spanish: "habitación", french: "chambre" },
+    { spanish: "el pasillo", french: "le couloir" },
+    { spanish: "cama", french: "lit" },
+    { spanish: "escritorio", french: "bureau" }, // (mueble)
+    { spanish: "ordenador", french: "ordinateur" }, // "PC" también se usa informalmente
+    { spanish: "monitor", french: "écran" }, // o "moniteur" (más técnico)
+    { spanish: "teclado", french: "clavier" },
+    { spanish: "los baños", french: "les toilettes" },
+    { spanish: "ratón (de ordenador)", french: "souris" } // ¡Sí, igual que el animal!
+];
