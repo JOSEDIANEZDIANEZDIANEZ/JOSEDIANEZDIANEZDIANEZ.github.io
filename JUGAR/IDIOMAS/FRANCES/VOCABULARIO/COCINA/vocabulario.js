f8be21d5b77a1a30af1ab37712daaa9f305525a6
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-05-04 13:12:23 +0200

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/VOCABULARIO/COCINA/vocabulario.js
@@ -0,0 +1,15 @@
+const vocabulary = [
+    { spanish: "cocina", french: "cuisine" },
+    { spanish: "frigorífico", french: "réfrigérateur" },
+    { spanish: "lavadora", french: "machine à laver" },
+    { spanish: "lavavajillas", french: "lave-vaisselle" },
+    { spanish: "horno", french: "four" },
+    { spanish: "microondas", french: "micro-ondes" },
+    { spanish: "fregadero", french: "évier" },
+    { spanish: "plato", french: "assiette" },
+    { spanish: "cuchara", french: "cuillère" },
+    { spanish: "cuchillo", french: "couteau" },
+    { spanish: "tenedor", french: "fourchette" },
+    { spanish: "cacerola", french: "casserole" },
+    { spanish: "sartén", french: "poêle" }
+];
