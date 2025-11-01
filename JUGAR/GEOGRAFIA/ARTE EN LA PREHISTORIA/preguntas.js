ce3e931220a93554bda4aef5e986b0fa801dc82e
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-01 23:23:28 +0100

    Add files via upload

---
+++ JUGAR/GEOGRAFIA/ARTE EN LA PREHISTORIA/preguntas.js
@@ -0,0 +1,76 @@
+const titulo = "EL ARTE EN LA PREHISTORIA";
+
+const preguntas = [
+  {
+    pregunta: "¿Cuándo comenzaron las primeras muestras de arte en la Prehistoria?",
+    respuesta: "En el Paleolítico."
+  },
+  {
+    pregunta: "¿Qué motivos tenían los primeros humanos para crear arte?",
+    respuesta: "Rituales mágicos y deseo de fertilidad."
+  },
+  {
+    pregunta: "¿Qué creían los humanos al pintar animales en el Paleolítico?",
+    respuesta: "Que sería más fácil cazarlos."
+  },
+  {
+    pregunta: "¿Qué tipo de arte se realiza en rocas y no se puede mover?",
+    respuesta: "Arte rupestre."
+  },
+  {
+    pregunta: "¿Cómo se llama el arte rupestre que aparece en las paredes de las cuevas?",
+    respuesta: "Arte parietal."
+  },
+  {
+    pregunta: "¿Qué ejemplo famoso tenemos de arte parietal?",
+    respuesta: "Las pinturas de Altamira."
+  },
+  {
+    pregunta: "¿Qué tipo de arte incluye esculturas o grabados en piedra, hueso, asta o marfil?",
+    respuesta: "Arte mobiliar."
+  },
+  {
+    pregunta: "¿Qué ejemplo famoso hay de arte mobiliar?",
+    respuesta: "Las Venus paleolíticas."
+  },
+  {
+    pregunta: "¿Qué tipo de arte suele ser la pintura en la Prehistoria?",
+    respuesta: "Arte rupestre."
+  },
+  {
+    pregunta: "¿Cuáles son los dos tipos de pintura prehistórica?",
+    respuesta: "Naturalista y esquemática."
+  },
+  {
+    pregunta: "¿Qué tipo de pintura apareció primero en el Paleolítico?",
+    respuesta: "La pintura naturalista."
+  },
+  {
+    pregunta: "¿Qué representaba la pintura naturalista?",
+    respuesta: "Animales que querían cazar."
+  },
+  {
+    pregunta: "¿Suele aparecer la figura humana en la pintura naturalista?",
+    respuesta: "No, rara vez aparece."
+  },
+  {
+    pregunta: "¿Qué tipo de pintura utiliza muchos colores?",
+    respuesta: "La pintura naturalista."
+  },
+  {
+    pregunta: "¿Cuándo apareció la pintura esquemática?",
+    respuesta: "En el Neolítico, hace unos 10 000 años."
+  },
+  {
+    pregunta: "¿Qué representaba la pintura esquemática?",
+    respuesta: "Escenas de caza con figuras humanas."
+  },
+  {
+    pregunta: "¿Qué tipo de pintura utiliza un solo color?",
+    respuesta: "La pintura esquemática."
+  },
+  {
+    pregunta: "¿Qué tipo de arte suele ser la escultura en la Prehistoria?",
+    respuesta: "Arte mobiliar."
+  }
+];
