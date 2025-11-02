290ef53cf76636c588d33fb16a7ef0d996a6cc52
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-02 19:59:36 +0100

    Add files via upload

---
+++ JUGAR/GEOGRAFIA/PREHISTORIA/HOMINIDOS/preguntas.js
@@ -0,0 +1,52 @@
+const titulo = "HOMINIDOS";
+
+const preguntas = [
+  {
+    pregunta: "¿Cuándo apareció el Australopitecus?",
+    respuesta: "Hace 4 millones de años."
+  },
+  {
+    pregunta: "¿Qué características tenía el Australopitecus?",
+    respuesta: "No fabricaban herramientas y eran bípedos."
+  },
+  {
+    pregunta: "¿Cuándo apareció el Homo Habilis?",
+    respuesta: "Hace 3 millones de años."
+  },
+  {
+    pregunta: "¿Qué características tenía el Homo Habilis?",
+    respuesta: "Fabricaban herramientas y se comunicaban."
+  },
+  {
+    pregunta: "¿Cuándo apareció el Homo Erectus?",
+    respuesta: "Hace 2 millones de años."
+  },
+  {
+    pregunta: "¿Qué características tenía el Homo Erectus?",
+    respuesta: "Controlaban el fuego y fueron los primeros en salir de África."
+  },
+  {
+    pregunta: "¿Cuándo apareció el Homo Neardental?",
+    respuesta: "Hace 1.5 millones de años."
+  },
+  {
+    pregunta: "¿Qué características tenía el Homo Neardental?",
+    respuesta: "Tenían un físico robusto y realizaban los primeros enterramientos."
+  },
+  {
+    pregunta: "¿Cuándo apareció el Homo Sapiens?",
+    respuesta: "Hace 1 millón de años."
+  },
+  {
+    pregunta: "¿Qué características tenía el Homo Sapiens?",
+    respuesta: "Fabricaban herramientas complejas y hacían arte."
+  },
+  {
+    pregunta: "¿Cuándo apareció el Homo Antecesor?",
+    respuesta: "Hace 800.000 años."
+  },
+  {
+    pregunta: "¿Qué características tenía el Homo Antecesor?",
+    respuesta: "Tenía rasgos parecidos al ser humano actual y evolucionó a partir del Homo Erectus."
+  }
+];
