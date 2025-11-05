f7fbde72ef5eddb7be0eff9d101eec35b40ad449
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-05 16:56:01 +0100

    Add files via upload

---
+++ JUGAR/MATEMATICAS/FACTORES/preguntas.js
@@ -0,0 +1,12 @@
+const titulo_del_juego = "CONCEPTOS GENERALES";
+
+const preguntas = [
+  { pregunta: "¿Cuándo un número es divisible por otro?", respuesta: "Cuando la división entre ellos es exacta", palabrasclave: "division,exacta" },
+  { pregunta: "¿Cuándo un número b es múltiplo de un número a?", respuesta: "Cuando la división de b entre a es exacta", palabrasclave: "division.exacta" },
+  { pregunta: "¿Cuándo un numero a es divisor de un numero b?", respuesta: "Cuando la división de b entre a es exacta", palabrasclave: "exacta" },
+  { pregunta: "¿Qué es un número primo?", respuesta: "Es un número que sólo se puede dividir por dos números, por si mismo y por uno", palabrasclave: "1,mismo" },
+  { pregunta: "¿Qué es un número compuesto?", respuesta: "Aquél que tiene más de dos divisores", palabrasclave: "2,divisores" },
+  { pregunta: "¿Qué es descomponer un número en factores?", respuesta: "Es expresarlo como una multiplicación de otros números", palabrasclave: "multiplicacion,otros,numeros" },
+  { pregunta: "¿Es lo mismo descomponer un número en factores que factorizar un número?", respuesta: "No. Sólo es factorización cuando la descomposición es en números primos", palabrasclave: "no" },
+  { pregunta: "¿Qué es factorizar?", respuesta: "Expresar un número como la multiplicación de números primos", palabrasclave: "multiplicación,primos" }
+];
