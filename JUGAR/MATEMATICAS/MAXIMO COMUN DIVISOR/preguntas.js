dd06ae99ec3dae18237fb254873a106b8563a8e3
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-05 16:55:28 +0100

    Add files via upload

---
+++ JUGAR/MATEMATICAS/MAXIMO COMUN DIVISOR/preguntas.js
@@ -0,0 +1,10 @@
+const titulo_del_juego = "MAXIMO COMUN DIVISOR";
+
+const preguntas = [
+  { pregunta: "¿Qué es el máximo común divisor, al comparar dos o más números?", respuesta: "Es el máximo divisor común a esos números", palabrasclave: "maximo,divisor,comun" },
+  { pregunta: "¿Cómo se calcula el máximo común divisor?", respuesta: "Factorizamos y multiplicamos los factores comunes elevados al menor exponente", palabrasclave: "factorizar,comunes,exponente" },
+  { pregunta: "Si un número es múltiplo de otro, ¿Cuál es el máximo común divisor de los dos números?", respuesta: "El menor de los dos números", palabrasclave: "menor" },
+  { pregunta: "¿Qué pasa con el máximo común divisor, si dos números no tienen divisores comunes?", respuesta: "Que el máximo común divisor será 1, y esos dos números son primos entre si.", palabrasclave: "divisor,1,primos" },
+  { pregunta: "¿Cómo se expresa máximo común divisor de dos números a y b?", respuesta: "Poniéndolos con la palabra mcd delante y los números entre paréntesis", palabrasclave: "parentesis" },
+  { pregunta: "¿Qué palabras nos dan pistas en un problema de que tenemos que utilizar el máximo común divisor?", respuesta: "Cuando nos piden que repartamos,dividamos cosas distintas sin que sobre nada", palabrasclave: "repartir,dividir,sobre" }
+];
