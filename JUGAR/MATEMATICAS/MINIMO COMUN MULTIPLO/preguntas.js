abf4e8720e81333f4b924c77c751e509655068ed
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-05 13:27:12 +0100

    Add files via upload

---
+++ JUGAR/MATEMATICAS/MINIMO COMUN MULTIPLO/preguntas.js
@@ -0,0 +1,8 @@
+const titulo_del_juego = "Matemáticas";
+
+const preguntas = [
+  { pregunta: "Definición de mínimo común múltiplo de dos o más números", respuesta: "Es el mínimo múltiplo en el que coinciden", palabrasclave: "minimo,multiplo,coinciden" },
+  { pregunta: "¿Cómo se calcula el mínimo común múltiplo?", respuesta: "Se factorizan los números a comparar y se cogen los comunes y no comunes elevados a la máxima potencia", palabrasclave: "factorizar,comunes,no,elevados,maxima" },
+  { pregunta: "¿Si un número es divisor de otro, cuál es el mínimo común múltiplo de ambos?", respuesta: "Sería el mayor de ellos", palabrasclave: "mayor" },
+  { pregunta: "¿Qué palabras en un problema matemático, me sugieren que estamos hablando de calcular el mínimo común múltiplo??", respuesta: "Cuando hablamos de palabras como : coinciden, se juntan, vuelven a encontrarse, repiten , ciclo, sincronizar...", palabrasclave: "coincidir,juntar,encontrar,sincronizar" }
+];
