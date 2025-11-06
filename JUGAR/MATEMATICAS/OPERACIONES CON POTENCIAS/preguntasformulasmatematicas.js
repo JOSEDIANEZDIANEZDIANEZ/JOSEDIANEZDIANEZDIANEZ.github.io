f5cb2922370ca2ba66abaf71817527a7433b948e
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-06 11:28:20 +0100

    Add files via upload

---
+++ JUGAR/MATEMATICAS/OPERACIONES CON POTENCIAS/preguntasformulasmatematicas.js
@@ -0,0 +1,32 @@
+const titulo_del_juego = "OPERACIONES CON POTENCIAS";
+
+const preguntas = [
+  {
+    pregunta: "¿Cuál es el resultado de una potencia con exponente 1?",
+    respuesta: "a¹ = a"
+  },
+  {
+    pregunta: "¿Cuál es el resultado de una potencia con exponente 0?",
+    respuesta: "a⁰ = 1 (si a ≠ 0)"
+  },
+  {
+    pregunta: "¿Cómo se calcula una potencia de una potencia?",
+    respuesta: "(aⁿ)ᵐ = aⁿˣᵐ"
+  },
+  {
+    pregunta: "¿Cómo se calcula la potencia de un producto?",
+    respuesta: "(a · b)ⁿ = aⁿ · bⁿ"
+  },
+  {
+    pregunta: "¿Cómo se calcula la potencia de un cociente?",
+    respuesta: "(a : b)ⁿ = aⁿ : bⁿ"
+  },
+  {
+    pregunta: "¿Cómo se realiza el producto de potencias con la misma base?",
+    respuesta: "aⁿ · aᵐ = aⁿ⁺ᵐ"
+  },
+  {
+    pregunta: "¿Cómo se realiza el cociente de potencias con la misma base?",
+    respuesta: "aⁿ : aᵐ = aⁿ⁻ᵐ"
+  }
+];
