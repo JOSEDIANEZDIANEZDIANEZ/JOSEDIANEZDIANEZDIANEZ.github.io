6a15af73822bb4777fa5d6181ebdbbc635e2f6b3
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-12 00:41:06 +0100

    Add files via upload

---
+++ JUGAR/BIOLOGIA/LOS MICROORGANISMOS/preguntas.js
@@ -0,0 +1,46 @@
+const titulo_del_juego = "LOS MICROORGANISMOS";
+
+const preguntas = [
+  {
+    pregunta: "¿Qué son los microorganismos?",
+    respuesta: "Seres vivos unicelulares o algunos pluricelulares que solo pueden verse con microscopio",
+    palabrasclave: "vivos,unicelulares,pluricelulares,microscopio"
+  },
+  {
+    pregunta: "¿Cómo se les llama también a los microorganismos?",
+    respuesta: "Microbios",
+    palabrasclave: "microbios"
+
+  },
+  {
+    pregunta: "¿Cuántos tipos de microorganismos hay?",
+    respuesta: "Cinco: levaduras, mohos, protozoos, algas unicelulares, bacterias",
+    palabrasclave: "levaduras,mohos,protozoo,algas,bacterias"
+  },
+  {
+  pregunta: "¿Cómo se les llama a los microorganismos perjudiciales?",
+  respuesta: "Patógenos",
+  palabrasclave: "patogenos"
+},
+{
+pregunta:"¿Cómo se les llama a las enfermedades causadas por los microorganísmos?",
+respuesta: "Enfermedades infecciosas y el microorganismo que causa la enfermedad se llama agente patógeno",
+palabrasclave: "infecciosas,agente,patogeno"
+
+},
+  {
+    pregunta: "¿Son perjudiciales todos los microorganismos?",
+    respuesta: "No, la mayoría son beneficiosos para el ser humano y el medioambiente.Como los microorganísmos productores y los descomponedores",
+    palabrasclave: "no,beneficiosos,productores,descomponedores"
+  },
+  {
+    pregunta: "¿Qué es un microorganismo productor y qué tipos lo son?",
+    respuesta: "Organismo que produce materia orgánica; los productores son algunas bacterias y las algas",
+    palabrasclave: "materia,organica,bacterias,algas"
+  },
+  {
+    pregunta: "¿Qué es un microorganismo descomponedor y qué tipos lo son?",
+    respuesta: "Organismo que transforma materia orgánica en inorgánica; los descomponedores son algunas bacterias, los mohos y las levaduras",
+    palabrasclave: "transforma,organica,inorganica,bacterias,mohos,levaduras"
+  }
+];
