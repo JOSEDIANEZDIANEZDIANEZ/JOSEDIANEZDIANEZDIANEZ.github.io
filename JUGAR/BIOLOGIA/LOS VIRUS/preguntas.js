a7e898b25a97de97b37102583dc452c2a2bbf240
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-12 11:03:45 +0100

    Add files via upload

---
+++ JUGAR/BIOLOGIA/LOS VIRUS/preguntas.js
@@ -0,0 +1,8 @@
+const titulo_del_juego = "LOS VIRUS";
+
+const preguntas = [
+  { pregunta: "¿Qué es un virus?", respuesta: "Son agentes microscópicos,más pequeños incluso que una célula. A diferencia de los microorganísmos no se consideran seres vivos ya que lo unico que pueden hacer es reproducirse y para ello necesitan una célula, por eso se llaman parásitos obligados.No pertenecen a ningun reino de los estudiados.", palabrasclave: "microscopicos,no,vivos,parasitos,obligados,reino" },
+  { pregunta: "Estructura de un virus", respuesta: "Tienen una estructura muy sencilla, formada sólo por una cubierta proteica, llamada cápsida, que contiene la información genética(ácido nucleico)", palabrasclave: "capsida,nucleico" },
+  { pregunta: "¿Los virus son siempre malos(patógenos)?", respuesta: "No. Tienen gran importancia biológica debido a varios factores. Al infectar a varias celulas promueven el intercambio genético y con ello la variabilidad de los organismos. Algunos virus como los virus bacteriófagos son capaces de atacar bacterias y de esa forma son un aliado en la lucha contra algunas bacterias", palabrasclave: "importancia,biologica,intercambio,genetico,variabilidad,bacteriofago,aliados" },
+  { pregunta: "Algunos virus", respuesta: "Los virus pueden atacar a todos los seres vivos. En el caso de las personas algunos virus son:el virus de la gripe,el sida y el covid-19", palabrasclave: "gripe,sida,covid" }
+];
