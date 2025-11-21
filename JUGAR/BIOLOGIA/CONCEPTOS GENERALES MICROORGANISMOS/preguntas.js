987c1612e3b446a63ca23da85eb8c87559de4938
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-21 13:27:17 +0100

    Add files via upload

---
+++ JUGAR/BIOLOGIA/CONCEPTOS GENERALES MICROORGANISMOS/preguntas.js
@@ -0,0 +1,11 @@
+const titulo_del_juego = "CONCEPTOS GENERALES MICROORGANISMOS";
+
+const preguntas = [
+  { pregunta: "¿QUÉ VAMOS A VER DE CADA MICROORGANISMO?(Y DE LOS VIRUS QUE NO SON MICROORGANISMOS)", respuesta: "De cada microorganismo y de los virus vamos a estudiar siete cosas en este orden.\nDefinición\nEstructura\nTres funciones(nutricion,reproducción,relación)\nClasificación\nEjemplos", palabrasclave: "definicion,estructura,nutricion,reproducion,relacion,clasificacion,ejemplos" },
+  { pregunta: "¿QUÉ SIGNIFICA LA FUNCIÓN DE RELACIÓN?", respuesta: "Se refiere al movimiento del microrganismo", palabrasclave: "movimiento" },
+  { pregunta: "LOS VIRUS¿PERTENECEN A ALGÚN REINO?", respuesta: "No, los virus no pertenecen a ningún reino", palabrasclave: "no" },
+  { pregunta: "¿QUÉ MICROORGANISMOS ESTÁN INCLUIDOS EN EL REINO MONERA?", respuesta: "Las bacterias y las arqueas", palabrasclave: "bacterias,arqueas" },
+  { pregunta: "¿QUÉ MICROORGANISMOS PERTENECEN AL REINO PROTISTA?", respuesta: "Los protozoos y las algas", palabrasclave: "protozoos,algas" },
+  { pregunta: "¿QUÉ MICROORGANISMOS PERTENECEN AL REINO FUNGI?", respuesta: "Los hongos", palabrasclave: "hongos" },
+  { pregunta: "LAS SETAS PERTENECEN A LOS HONGOS¿PERO SON MICROORGANISMOS?", respuesta: "No, porque son observables a simple vista", palabrasclave: "No" }
+];
