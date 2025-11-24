eb7dbd1d8c694430821a4ef9062ed14cc8015c6a
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-11-24 10:23:03 +0100

    Add files via upload

--- JUGAR/BIOLOGIA/LOS VIRUS/preguntas.js
+++ JUGAR/BIOLOGIA/LOS VIRUS/preguntas.js
@@ -1,13 +1,10 @@
-const titulo_del_juego = "MICROORGANISMOS";
+const titulo_del_juego = "VIRUS";
 
 const preguntas = [
-  { pregunta: "MICROORGANISMOS", respuesta: "Seres vivos. La mayoría unicelulares y algunos pluricelulares, que sólo son visibles al microscopio. También se les llama microbios.", palabrasclave: "vivos,unicelulares,pluricelulares,microscopio,microbios" },
-  { pregunta: "¿CUANDO SE DESCUBRIERON LOS MICROORGANISMOS?", respuesta: "Se descubrieron cuando empezaron a poder verse con la invención del microscopio, en el siglo XVII", palabrasclave: "microscopio,siglo" },
-  { pregunta: "TIPOS DE MICROORGANISMOS", respuesta: "Son cinco: \nAlgas unicelulares\nbacterias\nlevaduras,mohos(ambos son hongos) y\nprotozoos", palabrasclave: "algas,bacterias,levaduras,mohos,protozoos" },
-  { pregunta: "MICROORGANISMOS PATOGENOS", respuesta: "Son aquellos que producen enfermedades. Las enfermedades que producen se llaman enfermedades infecciosas y al microorganismo que causa la enfermedad se le llama agente patógeno.", palabrasclave: "infecciosas,patogeno" },
-  { pregunta: "EJEMPLOS DE ENFERMEDADES CAUSADAS POR MICROORGANISMOS PATOGENOS", respuesta: "Tuberculosis y cólera, causados por bacterias\nMalaria y enfermedad del sueño, causados por protozoos\nPie de atleta y candidiasis, causado por hongos", palabrasclave: "tuberculosis,colera,malaria,sueño,atleta,candidiasis" },
-  { pregunta: "MICROORGANISMOS BENEFICIOSOS", respuesta: "La mayoría de los microorganismos son beneficiosos, tanto para el ser humano, como para el medio ambiente.", palabrasclave: "humano,ambiente" },
-  { pregunta: "TIPOS DE MICROORGANISMOS BENEFICIOSOS", respuesta: "Hay dos tipos:\n-Productores, producen materia orgánica(algunas bacterias y algas)\n-Descomponedores, descomponen materia orgánica en inorgánica(algunas bacterias, mohos y levaduras)", palabrasclave: "productores,bacterias,algas,descomponedores,mohos,levaduras" },
-  { pregunta: "MICROORGANISMOS Y MEDIO AMBIENTE", respuesta: "Son imprescindibles para la vida en la tierra, participan en el ciclo de la materia, es decir la transformación de materia orgánica en inorgánica y vuelta a transformarla en orgánica ejemplo descomponen cadáveres. Algunos producen oxígeno, como las cianobacterias que son bacterias fotosintéticas y las algas", palabrasclave: "ciclo,oxigeno,cianobacterias" },
-  { pregunta: "UTILIZACION BENEFICIOSA DE LOS MICROORGANISMOS PARA EL SER HUMANO", respuesta: "Vamos a dar tres ejemplos:\n-Elaboración de alimentos(algunas bacterias o levaduras)\n-Producción de fármacos(la penicilina del hongo penicillium)\n-Investigación(Escherichia coli)", palabrasclave: "alimentos,farmacos,investigacion" }
+  { pregunta: "VIRUS,DEFINICION", respuesta: "No son células, son agentes microscópicos, que no son seres vivos ni pertenecen a ningún Reino. Están formados por cápsida y ácido nucleico", palabrasclave: "agentes,capsida" },
+  { pregunta: "VIRUS,NUTRICIÓN", respuesta: "No tienen", palabrasclave: "no" },
+  { pregunta: "VIRUS,REPRODUCCIÓN", respuesta: "Son parásitos obligados, necesitan una célula huésped para reproducirse", palabrasclave: "parasitos,obligados" },
+  { pregunta: "VIRUS,RELACIÓN(MOVIMIENTO)", respuesta: "No tienen función de relación.No tienen capacidad de movimiento propio.", palabrasclave: "no" },
+  { pregunta: "VIRUS,CLASIFICACIÓN", respuesta: "Son parásitos, pero ayudan algunas veces a la variabilidad genética y a acabar con algunas bacterias", palabrasclave: "variabilidad,bacterias" },
+  { pregunta: "VIRUS,EJEMPLOS", respuesta: "Covid-19,Sida,Gripe", palabrasclave: "sida,gripe" }
 ];
