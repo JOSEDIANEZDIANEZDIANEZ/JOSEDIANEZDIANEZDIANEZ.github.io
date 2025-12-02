9422c3fab26dfdce1dc16e4c04b06fcba24fb01f
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-12-02 11:45:02 +0100

    Add files via upload

---
+++ JUGAR/ALEXTUBE/videos.js
@@ -0,0 +1,50 @@
+const videos = [
+  {
+    "title": "NUREMBER",
+    "url": "https://www.youtube.com/watch?v=WvAy9C-bipY",
+    "thumbnail": "https://img.youtube.com/vi/WvAy9C-bipY/mqdefault.jpg",
+    "tema": "DOCUMENTALES"
+  },
+  {
+    "title": "Tensión ",
+    "url": "https://youtu.be/8SYSq051ej4?si=6YlTggtED6hL6rBR",
+    "thumbnail": "https://img.youtube.com/vi/8SYSq051ej4/mqdefault.jpg",
+    "tema": "TECNICAS DE ESTUDIO"
+  },
+  {
+    "title": "33 TRUCOS",
+    "url": "https://www.youtube.com/watch?v=1tl0i7cIkZA",
+    "thumbnail": "https://img.youtube.com/vi/1tl0i7cIkZA/mqdefault.jpg",
+    "tema": "TRUCOS DEL HOGAR"
+  },
+  {
+    "title": "ROBOCOP",
+    "url": "https://www.youtube.com/watch?v=SMyMVB6cQYQ",
+    "thumbnail": "https://img.youtube.com/vi/SMyMVB6cQYQ/mqdefault.jpg",
+    "tema": "CINE"
+  },
+  {
+    "title": "EL ILUSIONISTA",
+    "url": "https://www.youtube.com/watch?v=Ujr5jWNu3yE",
+    "thumbnail": "https://img.youtube.com/vi/Ujr5jWNu3yE/mqdefault.jpg",
+    "tema": "PELICULAS COMPLETAS"
+  },
+  {
+    "title": "TERMINATOR 2",
+    "url": "https://www.youtube.com/watch?v=MBShyOajLcg",
+    "thumbnail": "https://img.youtube.com/vi/MBShyOajLcg/mqdefault.jpg",
+    "tema": "CINE"
+  },
+  {
+    "title": "PHP 1",
+    "url": "https://www.youtube.com/watch?v=I75CUdSJifw&list=PLU8oAlHdN5BkinrODGXToK9oPAlnJxmW_",
+    "thumbnail": "https://img.youtube.com/vi/I75CUdSJifw/mqdefault.jpg",
+    "tema": "PROGRAMACION"
+  },
+  {
+    "title": "DOC HOLLYWOOD",
+    "url": "https://www.youtube.com/watch?v=6JBiFcMDefc",
+    "thumbnail": "https://img.youtube.com/vi/6JBiFcMDefc/mqdefault.jpg",
+    "tema": "PELICULAS COMPLETAS"
+  }
+];
