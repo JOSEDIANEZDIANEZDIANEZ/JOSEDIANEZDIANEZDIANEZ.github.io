4411c283989b18addca8fdce6a5e843676011cee
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2025-12-03 12:10:12 +0100

    Add files via upload

---
+++ JUGAR/BIOLOGIA/LAS PLANTAS/RELACION/preguntas.js
@@ -0,0 +1,29 @@
+const titulo_del_juego = "LAS PLANTAS Y SU FUNCION DE RELACION";
+
+const preguntas = [
+  
+
+  { 
+    pregunta: "CÓMO REALIZAN LA FUNCIÓN DE RELACIÓN LAS PLANTAS", 
+    respuesta: "Se relacionan con el entorno moviéndose(por tropismos o nastias) y mediante hormonas.", 
+    palabrasclave: "relacion,entorno,movimiento,hormonas" 
+  },
+
+  { 
+    pregunta: "HÁBLAME DE LOS TROPISMOS", 
+    respuesta: "Son movimientos permanentes y dependen de la dirección del estímulo. Pueden ser positivos (hacia el estímulo) o negativos (en contra). Tipos: fototropismo (luz), geotropismo (gravedad) e hidrotropismo (agua). Ejemplo: una raíz tiene geotropismo positivo, hidrotropismo positivo y fototropismo negativo.", 
+    palabrasclave: "tropismos,fototropismo,geotropismo,hidrotropismo,positivo,negativo" 
+  },
+
+  { 
+    pregunta: "HÁBLAME DE LAS NASTIAS", 
+    respuesta: "Son movimientos temporales y mucho más rápidos, no dependen de la dirección del estímulo.Tipos:fotonastia,el estímulo es la luz(como cuando se abre una flor cuando le da la luz del Sol),termonastia, el estímulo es la temperatura(como la del tulipán al abrir y cerrar la flor) o tigmonastia,el estímulo es la presión(como el cierre de las hojas de la mimosa)", 
+    palabrasclave: "nastias,movimientos,temporales" 
+  },
+
+  { 
+    pregunta: "HÁBLAME DE LAS HORMONAS EN LAS PLANTAS", 
+    respuesta: "Sustancias generadas por sus células que influyen en el crecimiento, la reproducción, la caída de hojas y la maduración de los frutos. Ejemplo: el etileno de los plátanos acelera la maduración de otros plátanos cercanos.", 
+    palabrasclave: "hormonas,crecimiento,reproduccion,hojas,frutos,etileno" 
+  }
+];
