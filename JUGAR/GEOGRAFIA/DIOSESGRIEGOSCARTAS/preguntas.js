ee3f0553e364fa47b41e44fb5887f963fd6ff23f
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2026-02-21 21:04:51 +0100

    Add files via upload

---
+++ JUGAR/GEOGRAFIA/DIOSESGRIEGOSCARTAS/preguntas.js
@@ -0,0 +1,16 @@
+const titulo_del_juego = "DIOSES GRIEGOS";
+const preguntas = [
+  { pregunta: "ATENEA", respuesta: "Diosa de la sabiduría. Minerva. Escudo y lanza.", palabrasclave: "sabiduria,minerva,escudo,lanza" },
+  { pregunta: "APOLO", respuesta: "Dios de las artes. Apolo. Lira, arcos y flechas.", palabrasclave: "artes,apolo,lira,arcos,flechas" },
+  { pregunta: "AFRODITA", respuesta: "Diosa del amor y la belleza. Venus. Paloma y concha.", palabrasclave: "amor,belleza,venus,paloma,concha" },
+  { pregunta: "ARTEMISA", respuesta: "Diosa de la caza. Diana. Arco y flechas.", palabrasclave: "caza,diana,arco,flechas" },
+  { pregunta: "HADES", respuesta: "Dios del infierno. Plutón. Perro de tres cabezas.", palabrasclave: "infierno,pluton,perro,cabezas" },
+  { pregunta: "HERA", respuesta: "Diosa del matrimonio y familia. Juno. Diadema y pavo real.", palabrasclave: "matrimonio,familia,juno,diadema,pavoreal" },
+  { pregunta: "HEFESTO", respuesta: "Dios del fuego y la metalurgia. Vulcano. Fragua y martillo.", palabrasclave: "fuego,metalurgia,vulcano,fragua,martillo" },
+  { pregunta: "POSEIDON", respuesta: "Dios del mar. Neptuno. Tridente.", palabrasclave: "mar,neptuno,tridente" },
+  { pregunta: "HESTIA", respuesta: "Diosa del hogar. Vesta. Fuego sagrado.", palabrasclave: "hogar,vesta,fuego,sagrado" },
+  { pregunta: "DEMETER", respuesta: "Diosa de la agricultura. Ceres. Grano.", palabrasclave: "agricultura,ceres,grano" },
+  { pregunta: "ZEUS", respuesta: "Padre de todos los dioses. Júpiter. Rayo y águila.", palabrasclave: "padre,jupiter,rayo,aguila" },
+  { pregunta: "HERMES", respuesta: "Mensajero de los dioses. Mercurio. Casco y sandalias aladas.", palabrasclave: "mensajero,mercurio,casco,sandalias" },
+  { pregunta: "ARES", respuesta: "Dios de la guerra. Marte. Casco, armadura y armas.", palabrasclave: "guerra,marte,casco,armadura,armas" }
+];
