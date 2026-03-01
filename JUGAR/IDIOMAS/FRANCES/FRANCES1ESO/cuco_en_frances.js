6512b9527653b60a1767e06732bd683ba9e9ecff
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2026-03-01 23:08:39 +0100

    Add files via upload

---
+++ JUGAR/IDIOMAS/FRANCES/FRANCES1ESO/cuco_en_frances.js
@@ -0,0 +1,127 @@
+var preguntas = [
+    {	"carta":"Nom",
+        "pregunta": "Comment tu t'appelles ?",
+        "respuesta": "¿Cómo te llamas?"
+    },
+    {
+	"carta":"Nom",
+        "pregunta": "Je m'appelle Alejandro",
+        "respuesta": "Yo me llamo Alejandro"
+    },
+    {
+	"carta":"Nationalité",
+        "pregunta": "Quelle nationalité tu as ?",
+        "respuesta": "¿Qué nacionalidad tienes?"
+    },
+    {	
+	"carta":"Nationalité",
+        "pregunta": "Je suis espagnol",
+        "respuesta": "Yo soy español"
+    },
+    {
+	"carta":"âge",
+        "pregunta": "Quel âge tu as ?",
+        "respuesta": "¿Qué edad tienes?"
+    },
+    {
+	"carta":"âge",
+        "pregunta": "J'ai douze ans",
+        "respuesta": "Tengo doce años"
+    },
+    {
+	"carta":"Lieu/habiter",
+        "pregunta": "Où tu habites ?",
+        "respuesta": "¿Dónde vives?"
+    },
+    {
+	"carta":"Lieu/habiter",
+        "pregunta": "J'habite à Séville, en Espagne",
+        "respuesta": "Vivo en Sevilla, en España"
+    },
+    {
+	"carta":"Profession",
+        "pregunta": "Qu'est-ce que tu fais dans la vie?",
+        "respuesta": " ¿Qué haces en la vida?"
+    },
+    {
+	"carta":"Profession",
+        "pregunta": "Je suis étudiant",
+        "respuesta": "Soy estudiante"
+    },
+    {
+	"carta":"caractère",
+        "pregunta": "Comment est ton caractère ?",
+        "respuesta": "¿Cómo es tu carácter?"
+    },
+    {
+	"carta":"caractère",
+        "pregunta": "Je suis travailleur et athlète",
+        "respuesta": "Soy trabajador y deportista"
+    },
+
+
+
+
+
+
+    {
+	"carta":"Date",
+        "pregunta": "Quelle est la date d'aujourd'hui ?",
+        "respuesta": "¿Cuál es la fecha de hoy?"
+    },
+    {
+	"carta":"Date",
+        "pregunta": "Aujourd'hui c'est le mardi trois mars",
+        "respuesta": "Hoy es martes tres de marzo"
+    },
+    {
+	"carta": "Anniversaire",
+        "pregunta": "C'est quand ton anniversaire ?",
+        "respuesta": "¿Cuándo es tu cumpleaños?"
+    },
+    {
+	"carta": "Anniversaire",
+        "pregunta": "Mon anniversaire c'est le onze mars",
+        "respuesta": "Mi cumpleaños es el once de marzo"
+    },
+    {
+	"carta":"Demander l'identité de quelqu'un.Preguntar la identidad de alguien",
+        "pregunta": "Qui est-ce ?",
+        "respuesta": "¿Quién es?"
+    },
+    {
+	"carta":"Demander l'identité de quelqu'un.Preguntar la identidad de alguien",
+	"pregunta": "C'est Javi / Ce sont Javi et Elena",
+        "respuesta": "Es Javi / Son Javi y Elena"
+    },
+    {
+	"carta": "Demander l'identité de quelque chose,preguntar la identaidad de cualquier cosa",
+        "pregunta": "Qu'est-ce que c'est ?",
+        "respuesta": "¿Qué es esto?"
+    },
+    {
+	"carta": "Demander l'identité de quelque chose,preguntar la identaidad de cualquier cosa",
+        "pregunta": "C'est un stylo / Ce sont des livres",
+        "respuesta": "Es un bolígrafo / Son libros"
+    },
+    {	
+	"carta":"Les goûts",
+        "pregunta": "Qu'est-ce que tu aimes ?",
+        "respuesta": "¿Qué te gusta?"
+    },
+    {
+	"carta":"Les goûts",
+        "pregunta": "J'aime la lecture et les jeux vidéo",
+        "respuesta": "Me gusta la lectura y los videojuegos"
+    },
+    {
+	"carta":"La famille",
+        "pregunta": "Mon père s'appelle Javier, il travaille à Carrefour",
+        "respuesta": "Mi padre se llama Javier, él trabaja en Carrefour"
+    },
+    {
+	"carta":"La famille",
+        "pregunta": "Ma mère s'appelle Virginia, elle est fonctionnaire",
+        "respuesta": "Mi madre se llama Virginia, ella es funcionaria"
+    }
+];
