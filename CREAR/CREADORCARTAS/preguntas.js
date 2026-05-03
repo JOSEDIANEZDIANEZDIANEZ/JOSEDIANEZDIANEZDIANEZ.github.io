a2e75b07779313cbcf9af48a9cbe844e05b0de03
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2026-05-03 13:47:14 +0200

    Add files via upload

---
+++ CREAR/CREADORCARTAS/preguntas.js
@@ -0,0 +1,14 @@
+const titulo_del_juego = "CIENCIAS NATURALES";
+
+const preguntas = [
+  {
+    pregunta: "QUIEN DESCUBRIO AMERICA",
+    respuesta: "CRISTOBAL COLON",
+    palabrasclave: "quien,descubrio,america,cristobal,colon"
+  },
+  {
+    pregunta: "QUIEN EMPEZO LA SEGUNDA GUERRA MUNDIAL",
+    respuesta: "hitler",
+    palabrasclave: "quien,empezo,segunda,guerra,mundial"
+  }
+];
