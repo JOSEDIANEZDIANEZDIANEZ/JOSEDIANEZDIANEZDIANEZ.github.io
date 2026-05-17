73bff1461d8d0a0509632699b5e548ced5613e6a
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2026-05-18 00:17:46 +0200

    Add files via upload

---
+++ CREAR/CREADORCARTASMEJORADO2/preguntas.js
@@ -0,0 +1,23 @@
+const titulo_del_juego = "diseño web";
+
+const preguntas = [
+
+  {
+    pregunta: "Que es html",
+    respuesta: "Es como la estructura donde va todo lo demás de una pagina de internet",
+    palabrasclave: "estructura"
+  }
+,
+  {
+    pregunta: "Que es css",
+    respuesta: "Es lo que le da el diseño a la pagina",
+    palabrasclave: "diseño"
+  }
+,
+  {
+    pregunta: "Que es javascript?",
+    respuesta: "Es lo que le da funcionalidad a  la pagina",
+    palabrasclave: "funcionalidad"
+  }
+
+];
