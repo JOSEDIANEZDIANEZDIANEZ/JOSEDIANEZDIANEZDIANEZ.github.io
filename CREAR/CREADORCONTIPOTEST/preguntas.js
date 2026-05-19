8cc64a81e79cdaec0fdee2aec763edf8322245a8
JOSEDIANEZDIANEZDIANEZ <comoaprenderjugando@gmail.com>
2026-05-19 10:30:20 +0200

    Add files via upload

---
+++ CREAR/CREADORCONTIPOTEST/preguntas.js
@@ -0,0 +1,26 @@
+const titulo_del_juego = "sanchez";
+
+const preguntas = [
+
+  {
+    pregunta: "¿De que color es el caballo blanco de Santiago?",
+    respuesta: "Blanco",
+    palabrasclave: "blanco",
+    respuesta_incorrecta1: "El pp",
+    respuesta_incorrecta2: "ninguna"
+  },
+  {
+    pregunta: "¿Quien ha ganado las elecciones?",
+    respuesta: "El pp",
+    palabrasclave: "pp",
+    respuesta_incorrecta1: "ninguna",
+    respuesta_incorrecta2: "Blanco"
+  },
+  {
+    pregunta: "¿Tiene solución este mundo?",
+    respuesta: "ninguna",
+    palabrasclave: "ninguna",
+    respuesta_incorrecta1: "Blanco",
+    respuesta_incorrecta2: "El pp"
+  }
+];
